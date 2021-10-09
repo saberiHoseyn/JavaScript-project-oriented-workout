763d50b28d4fcf4a4cd057cd31c8e00998e8ed5c
saberi.hoseyn <saberi.hoseyn.hs@gmail.com>
2021-10-09 17:37:59 +0330

    codes refactoring and Improvement .

--- project-1/script.js
+++ project-1/script.js
@@ -1,53 +1,92 @@
 let inputConter = document.querySelector("#input-conter");
-let startCounter = document.querySelector("#start-counter");
-let errorElement = document.querySelector("#error-message");
-let timerCircle = document.querySelector(".c100");
 let startBox = document.querySelector(".start-box");
+let startCounter = document.querySelector("#start-counter");
+let timerCircle = document.querySelector(".c100");
 let timerNum = document.querySelector(".c100 > span");
-let loadingMessage = document.querySelector(".message .loading");
-let successMessage = document.querySelector(".message .success");
+let lastPersent = "p100";
+let originSeconds , seconds, timerId;
+
+
 
 startCounter.addEventListener("click" , function(e) {
-    let seconds = parseInt(inputConter.value);
+    seconds = parseInt(inputConter.value);
     
-    if(isNaN(seconds)){
+    if(isNaN(seconds)) return  toggleErrorMessage( { show : true , message : "زمان را به درستی وارد کنید" });
+
+
+    toggleErrorMessage({show : false});
+    toggleStartBox({show : false});
+    toggleLoadingMessage({show : true});
+    toggleTimer({show : true , seconds});
+
+
+    originSeconds = seconds;
+    timerId = setInterval( startTimer , 1000);
+});
+
+
+
+
+let toggleErrorMessage = ({show , message}) =>{
+    let errorElement = document.querySelector("#error-message");
+
+    if(show){
         errorElement.textContent = "زمان را به درستی وارد کنید";
         errorElement.classList.add("active");
-        return;
+    }else {
+        errorElement.classList.remove("active");
+    };
+};
+
+let toggleStartBox = ({show}) =>{
+    if(show) {
+        startBox.style.display = "block";
+        inputConter.value = "";
+    }else {
+        startBox.style.display = "none";
+    };
+};
+
+let toggleLoadingMessage = ({show}) =>{
+    let loadingMessage = document.querySelector(".message .loading");
+    let successMessage = document.querySelector(".message .success");
+
+    if(show) {
+        loadingMessage.style.display = "block";
+        successMessage.style.display = "none";
+    }else {
+        loadingMessage.style.display = "none";
+        successMessage.style.display = "block";
+    };
+};
+
+let startTimer = () => {
+    if(lastPersent) timerCircle.classList.remove(lastPersent);
+
+
+    if(seconds <= 0 ) {
+        clearInterval(timerId);
+        toggleStartBox({show : true});
+        toggleLoadingMessage({show : false})
+        toggleTimer({show : false});
+        return
     };
 
-    errorElement.classList.remove("active");
-    timerCircle.style.display = "block";
-    startBox.style.display = "none";
+    seconds -= 1;
     timerNum.textContent = seconds;
-    loadingMessage.style.display = "block";
-    successMessage.style.display = "none";
 
+    lastPersent = `p${Math.abs(Math.floor((((originSeconds - seconds) / originSeconds) * 100) - 100))}`;
+    timerCircle.classList.add(lastPersent);
+};
 
-    let originSeconds = seconds;
-    let lastPersent = "p100";
-
-    let timerId = setInterval( () => {
-        if(lastPersent) timerCircle.classList.remove(lastPersent);
-
-        if(seconds <= 0 ) {
-            clearInterval(timerId);
-            startBox.style.display = "block";
-            timerCircle.style.display = "none";
-            loadingMessage.style.display = "none";
-            successMessage.style.display = "block";
-            inputConter.value = "";
-            timerCircle.classList.add("p100");
-            return
-        };
-
-
-
-        seconds -= 1;
-        let persent = Math.abs(Math.floor((((originSeconds - seconds) / originSeconds) * 100) - 100));
-        console.log(persent);
-        lastPersent = `p${persent}`;
-        timerCircle.classList.add(lastPersent);
+let toggleTimer = ({show , seconds}) =>{
+    if(show) {
+        timerCircle.style.display = "block";
         timerNum.textContent = seconds;
-    } , 1000);
-});
+    }else {
+        timerCircle.style.display = "none";
+        timerCircle.classList.remove(lastPersent);
+        lastPersent = "p100";
+        timerCircle.classList.add(lastPersent);
+    };
+};
